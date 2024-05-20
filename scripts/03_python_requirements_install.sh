a3572a663e3e64b1294fbb9de8a484b438e14d94
Carlos Lopez <celm1990@gmail.com>
2024-05-20 12:30:03 -0500

    [IMP] actualizar version de copier a 9.2.0

--- scripts/03_python_requirements_install.sh
+++ scripts/03_python_requirements_install.sh
@@ -2,6 +2,6 @@
 
 virtualenv /opt/odoo/venv -p /usr/bin/python3
 source /opt/odoo/venv/bin/activate
-pip3 install copier==9.1.0 invoke pre-commit
+pip3 install copier==9.2.0 invoke pre-commit
 echo -e "\n--- IMPORTANTE: active el entorno virtual con el comando:"
 echo -e "source /opt/odoo/venv/bin/activate"
